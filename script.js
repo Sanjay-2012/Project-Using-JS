0700024f252d6cec60ab74bdd8597ecf36e04361
Sanjay <sanjaysaravanan200420@gmail.com>
2025-08-09 14:49:41 +0530

    Implemented category fetch and display from Mealdb API

--- script.js
+++ script.js
@@ -19,6 +19,29 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 
+  // Load categories
+  fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    .then(res => res.json())
+    .then(data => {
+      data.categories.forEach(category => {
+       
+        // sidebar elements
+        const li = document.createElement("li");
+        li.textContent = category.strCategory;
+        li.addEventListener("click", () => showCategoryDetails(category));
+        categoryList.appendChild(li);
+
+        // Create card in main 
+        const card = document.createElement("div");
+        card.classList.add("category-card");
+        card.innerHTML = `
+          <img src="${category.strCategoryThumb}" />
+          <p>${category.strCategory}</p>
+        `;
+        card.addEventListener("click", () => showCategoryDetails(category));
+        categoriesContainer.appendChild(card);
+      });
+    });
 
 
 
